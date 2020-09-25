7f0905fbe4104c5958789572a2b4c3ee05d085fb
omzi <obiohaomezibe@gmail.com>
2020-09-25 18:54:33 +0100

    Cleaned up server.js

--- server.js
+++ server.js
@@ -6,30 +6,21 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
-
-app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
 require('isomorphic-fetch');
 
 app.get('/results.json', (req, res) => {
-  fetch('https://jsmx-leaderboard.herokuapp.com/leaderboard/results.json')
-	.then(response => response.json())
-	.then(data => {
-    res.status(200);
-    res.send(data);
-  })
-  .catch((error) => {
-    res.status(500);
-    res.send({
-      code: 500,
-      errorType: error.name,
-      errorMessage: error.name + ' : ' + error.message
+  fetch("https://jsmx-leaderboard.herokuapp.com/leaderboard/results.json")
+    .then(res => res.json())
+    .then((data) => {
+      res.status(200).send(data)
     })
-  })
+    .catch((error) => {
+      res.status(500).send({ code: 500, errorType: error.name })
+    })
 })
 
 app.use((req, res, next) => {
-  res.status(404).end(fs.readFileSync(path.join(__dirname, '/public/404.html'), { encoding: 'utf8'}))
+  res.status(404).end(fs.readFileSync(path.join(__dirname, '/public/404.html'), { encoding: 'utf8' }))
 })
